e1e5982fdb56f3166e5ff15a2c221d883109f229
emittman <emittman@iastate.edu>
2016-05-25 14:30:14 -0500

    Initial commit
    First go at using mpi compiler.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+
+int main(){
+  int N = 10;
+  
+  std::vector<int> vec(N);
+  
+#pragma omp parallel for
+  for(int i=0; i<N; i++)
+    vec[i] = i * i;
+  
+  #pragma omp parallel for
+  for(int i=0; i<6; i++){
+    std::cout << "In thread " << i << " the value is: " << vec[i] << ".\n\n";
+  }
+  
+  return 0;
+}
