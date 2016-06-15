af8c2804a9e22f6cffce8f1bf7b3cfa222259565
emittman <emittman@iastate.edu>
2016-06-15 13:23:02 -0500

    Fix bad memory access

--- src/draw_theta.h
+++ src/draw_theta.h
@@ -6,16 +6,16 @@
 #include "cluster_stats.h"
 #include "sample_wrappers.h"
 
-void draw_theta(fvec &beta, double *sigma2, uvec &z, double *yTy, fvec &yTx, fvec &xTx, fvec &Gk, int G, int K, int V, int n){
+void draw_theta(fvec &beta, double *sigma2, uvec &z, double *yTy, fvec &xTy, fvec &xTx, fvec &Gk, int G, int K, int V, int n){
   fvec IGscale(K);
   //private k?  
-  #pragma omp parallel for
+  // #pragma omp parallel for
   for(int k = 0; k<K; k++){
     fvec beta_hat(V);
     fvec chol_S(V*V);
     fvec beta_raw(V);
-    cluster_stats(k, yTx, xTx, G, V, n, z, Gk.begin() + k, beta_hat, chol_S, IGscale.begin() + k);
-    fveci beta_k_iter = beta.begin() + V*K;
+    cluster_stats(k, xTy, xTx, G, V, n, z, Gk.begin() + k, beta_hat, chol_S, IGscale.begin() + k);
+    fveci beta_k_iter = beta.begin() + V*k;
     for(int v=0; v<V; v++)
       beta_raw[v] = rnorm(0, 1); //draw beta_raw
     
