e377f15746f6b8c2fcf1d3e44ed9a7858523aa03
emittman <emittman@iastate.edu>
2016-07-05 11:28:13 -0500

    Generalize function for posterior predictive plot

--- notes/mcmc_postprocess.R
+++ notes/mcmc_postprocess.R
@@ -1,4 +1,4 @@
 source("plot_mcmc.R")
 
 mcmc <- readRDS("samples_with_inits.rds")
-make_ppplot_for_P(mcmc = mcmc, dim1 = 1, dim2 = 2, name1 = "X1", name2 = "X2", plotfilename = "ppplot_testrun")
+make_ppplot_for_P(mcmc = mcmc, dims=1, names="beta", plotfilename = "ppplot_testrun")
