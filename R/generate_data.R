3d1ab199409511eef440a30a66eb5a4b75c6d0b8
emittman <emittman@iastate.edu>
2016-07-16 00:15:38 -0500

    Allow sigma2 to be user specifed when generating data.

--- R/generate_data.R
+++ R/generate_data.R
@@ -9,7 +9,7 @@
 #' 
 #' @export
 
-generate_data <- function(X, N, G, K, beta=NULL, pi=NULL){
+generate_data <- function(X, N, G, K, beta=NULL, pi=NULL, sigma2=1.0){
   if(is.null(dim(X)) || dim(X)[1] != dim(X)[2]) stop("X must be a square matrix")
   V <- dim(X)[1]
   if(!is.null(beta)){
@@ -25,7 +25,7 @@ generate_data <- function(X, N, G, K, beta=NULL, pi=NULL){
   Xexpand <- matrix(rep(X, each=N), V*N, V)
   groupMeans <- Xexpand %*% beta
   z <- sample(1:K, G, replace=T, prob = pi)
-  y <- t(round(sapply(z, function(zz) rnorm(V*N, groupMeans[,zz])), 3))
+  y <- t(round(sapply(z, function(zz) rnorm(V*N, groupMeans[,zz], sigma2)), 3))
   list(G = G,
        V = V,
        N = N,
